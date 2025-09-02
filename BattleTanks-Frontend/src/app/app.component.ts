108f97c9b8ca8bbbf341e1b49b355b8a8aa3148c
Rodrigo Machaca Aruquipa <Rodrigo.Machaca@jala.university>
2025-09-02 09:30:53 -0400

    feat: add game room service

--- BattleTanks-Frontend/src/app/app.component.ts
+++ BattleTanks-Frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'tank-game';
